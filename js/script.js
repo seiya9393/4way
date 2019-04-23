e9ed0dd74bb501467562931174c3f9443faad011
seiya9393 <saitohseiya9393@gmail.com>
2019-04-23 14:43:58 +0000

    wrap

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(function(){
-  
+  $('body').fadeIn(1500);
+
+  $(".asn a").append("<img class='cont_art' src='./img/asunojokei_logo.png' alt='明日の叙景' >");
+  $(".pale a").append("<img class='cont_art' src='./img/pale_logo.png' alt='pale' >");
+  $(".nhom a").append("<img class='cont_art' src='./img/nhomme_logo.png' alt='nhomme' >");
+  $(".tcks a").append("<img class='cont_art' src='./img/tcks_logo.png' alt='冬蟲夏草' >");
 });
